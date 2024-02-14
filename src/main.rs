bcb8e1a8b156e9134fc50f86e9ee81c9d12c5aea
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2024-02-14 18:43:11 -0300

    Check if recent pgxn releases are already in Trunk

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::fs;
+pub type Result<T = ()> = anyhow::Result<T>;
+
+use std::{cmp::Ordering, fs};
 
 use once_cell::sync::Lazy;
 use reqwest::Client;
 use tempfile::tempdir;
 
 use dist::get_dists;
-
-use crate::trunk::build_project;
+use trunk::fetch_contrib_entries;
 
 static CLIENT: Lazy<Client> = Lazy::new(|| Client::new());
 
@@ -15,20 +16,35 @@ mod dist;
 mod trunk;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result {
     let tmp_dir = tempdir()?;
 
-    let dists = get_dists().await.unwrap();
+    let (entries, dists) = tokio::try_join!(fetch_contrib_entries(), get_dists())?;
 
     for release in dists.recent {
-        // TODO: check if extension is already in Trunk
+        // Check if extension is already in Trunk
+        let maybe_trunk_entry = entries.iter().find(|entry| {
+            entry.extension.name == release.dist
+                || entry.extension.extension_name.as_deref() == Some(&release.dist)
+        });
+
+        if let Some(trunk_entry) = maybe_trunk_entry {
+            println!("Already in Trunk: {}", trunk_entry.extension.name);
+            if compare_by_semver(&release.version, &trunk_entry.extension.version)
+                == Ordering::Greater
+            {
+                // Means pgxn has a more updated version of this extension compared to trunk
+            } else {
+                // Trunk has a more or equally updated version of this extension, so skip
+                println!("Already updated in Trunk: {}", trunk_entry.extension.name);
+                continue;
+            }
+        }
+
         let extracted_path = release.download_to(tmp_dir.path()).await?;
         println!("Extracted {}", extracted_path.display());
 
-        if let Err(err) = build_project(&release, &extracted_path).await {
-            eprintln!("Error: {err}");
-            continue;
-        }
+        // Create Dockerfile
 
         // TODO: open PR
 
@@ -36,4 +52,27 @@ async fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+fn compare_by_semver(a: &str, b: &str) -> Ordering {
+    let a_parts: Vec<i32> = a
+        .split('.')
+        .map(|p| p.parse::<i32>().unwrap_or(0))
+        .collect();
+    let b_parts: Vec<i32> = b
+        .split('.')
+        .map(|p| p.parse::<i32>().unwrap_or(0))
+        .collect();
+
+    let len = std::cmp::min(a_parts.len(), b_parts.len());
+
+    for i in 0..len {
+        match a_parts[i].cmp(&b_parts[i]) {
+            Ordering::Greater => return Ordering::Greater,
+            Ordering::Less => return Ordering::Less,
+            Ordering::Equal => continue,
+        }
+    }
+
+    a_parts.len().cmp(&b_parts.len())
 }
--- src/trunk.rs
+++ src/trunk.rs
@@ -1,32 +1,76 @@
-use std::{ops::Not, path::Path};
+use std::io::{Cursor, Read};
+use std::ops::Not;
 
-use anyhow::bail;
-use tokio::process::Command;
+use crate::Result;
+use anyhow::Context;
+use flate2::read::GzDecoder;
+use serde::Deserialize;
+use tar::EntryType;
 
-use crate::dist::Release;
+pub async fn fetch_contrib_entries() -> Result<Vec<TrunkToml>> {
+    let url = "https://github.com/tembo-io/trunk/archive/refs/heads/main.tar.gz";
+    let mut tomls = Vec::new();
 
-pub async fn build_project(release: &Release, source_dir: &Path) -> anyhow::Result<()> {
-    let Release { dist, version, .. } = release;
+    let trunk_archive = reqwest::get(url).await?.bytes().await?;
 
-    let exit_status = Command::new("trunk")
-        .current_dir(source_dir)
-        .args([
-            "build",
-            "--name",
-            dist,
-            "--version",
-            version,
-            "--pg-version",
-            "15",
-            "--output-path",
-            &*source_dir.to_string_lossy(),
-        ])
-        .status()
-        .await?;
+    // Decompress .gz
+    let mut buf = Vec::with_capacity(trunk_archive.len() * 8);
+    GzDecoder::new(&*trunk_archive).read_to_end(&mut buf)?;
 
-    if exit_status.success().not() {
-        bail!("Problem building {dist}-{version} for Pg15!");
+    // buf now contains the contents of the tar archive
+    let mut archive = tar::Archive::new(Cursor::new(buf));
+
+    for maybe_entry in archive.entries()? {
+        let mut entry = maybe_entry?;
+        let header = entry.header();
+        let entry_size = header.entry_size().unwrap_or(500);
+
+        let EntryType::Regular = header.entry_type() else {
+            continue;
+        };
+
+        let path = entry.path()?;
+
+        let Some(parent) = path.parent() else {
+            continue;
+        };
+
+        // Keep only paths within a directory within contrib
+        // E.g.: `trunk-main/contrib/postgis/Trunk.toml`
+        if parent
+            .components()
+            .rev()
+            .nth(1)
+            .map(|component| component.as_os_str() == "contrib")
+            .unwrap_or(false)
+            .not()
+        {
+            continue;
+        }
+
+        if path.file_name().unwrap() == "Trunk.toml" {
+            let contents = {
+                let mut buf = Vec::with_capacity(entry_size as usize);
+
+                entry.read_to_end(&mut buf)?;
+                String::from_utf8(buf).with_context(|| "Trunk.toml was not valid UTF-8")?
+            };
+
+            tomls.push(toml::from_str(&contents)?)
+        }
     }
 
-    Ok(())
+    Ok(tomls)
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TrunkToml {
+    pub extension: ExtensionInfo,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ExtensionInfo {
+    pub name: String,
+    pub extension_name: Option<String>,
+    pub version: String,
 }
